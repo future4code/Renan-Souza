1d6bfefbdf3d172224192b9e592a40b1febb0a65
Renan Souza <dom_fullstack@icloud.com>
2021-08-11 15:21:09 -0300

    exercicio 1 feito

---
+++ semana1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,7 @@
+
+const nomeUsuario = prompt('Digite o seu nome:')
+const email = prompt('Digite o seu e-mail:')
+
+console.log('O e-mail ' + email +' foi cadastrado com sucesso. Seja bem-vinda(o), '  + nomeUsuario);
+
+console.log('teste')
