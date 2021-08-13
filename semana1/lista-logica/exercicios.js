1afa97d626c26f904c3a645418df0df42e3c9227
Renan Souza <dom_fullstack@icloud.com>
2021-08-13 19:00:50 -0300

    Projeto atualizado até 8

--- semana1/lista-logica/exercicios.js
+++ semana1/lista-logica/exercicios.js
@@ -25,44 +25,56 @@ function calculaAreaRetangulo() {
 
 // EXERCÍCIO 02
 function imprimeIdade() {
-  // implemente sua lógica aqui
+  let anoAtual = Number(prompt('Em que ano estamos?'))
+  let anoNascimento = Number(prompt('Em que ano você nasceu?'))
+  let idade =anoAtual - anoNascimento
+  console.log(idade)
 
 }
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
-  // implemente sua lógica aqui
+  let imc = peso/(altura*altura)
+  return imc 
 
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
-  // implemente sua lógica aqui
-  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+  let nomeUsuario = prompt('Qual é o seu nome?')
+  let idadeUsuario = Number(prompt('Qual é a sua idade?'))
+  let emailUsuario = prompt('Qual é o seu email')
+console.log(`Meu nome é ${nomeUsuario}, tenho ${idadeUsuario} anos, e o meu email é ${emailUsuario}.`)
 }
+  
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
-  // implemente sua lógica aqui
+  let cor1 = prompt('Qual a sua primeira cor favorita')
+  let cor2 = prompt('Qual a sua segunda cor favorita')
+  let cor3 = prompt('Qual a sua terceira cor favorita')
+  let coresFavoritas = [cor1, cor2, cor3]
+  console.log(coresFavoritas)
+  
 
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
-  // implemente sua lógica aqui
-
+  console.log(string.toUpperCase())
+  return string.toUpperCase()
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
-  // implemente sua lógica aqui
-
+  let custoIngresso = (custo/valorIngresso)
+  return custoIngresso
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
-  // implemente sua lógica aqui
+  let comparacao = string1.length === string2.length
+  return comparacao
 
 }
 
