408504dde52d6c8d93e7792ad563cd04370f8bd1
Renan Souza <dom_fullstack@icloud.com>
2021-08-17 16:42:18 -0300

    Exercicios

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,46 @@
+// Exercicio de Interpretação
+
+// 1 a) O código pede para que o usuário insira um número e Se esse número for % 2 === 0 passará no teste caso não passará.
+// b) Para números que % 2 ===0
+// c) Para números que % 2 não é ===0
+
+// 2 a) Para precificar as frutas pré-seleciondas e caso não esteja na lista o valor será R$ 5.00
+// b) O valor da fruta maça será R$ 2.25
+// c) Imprimiria o valor de R$ 5.00, ou seja imprimiria o valor do default.
+
+// 3 a) Solicitando que o usuário insira um numero
+// b) Apresentaria a mensagem... Esse número passou no teste. Caso digita-se -10 apresentaria erro
+
+// Exercícios de escrita de código
+
+// //1) 
+// let idadeUsuario = Number(prompt('Digite sua idade'))
+
+// if (idadeUsuario >= 18) {
+//     console.log('Você pode dirigir')
+// } else {
+//     console.log('Você não pode dirigir.') 
+// }
+
+// //2)
+
+let turnoAluno = prompt('Digite o seu turno, sendo: M (matutino), V (Vespertino) ou N (Noturno)')
+
+if(turnoAluno === 'M'){
+    console.log('Bom dia!')
+    } else if(turnoAluno === 'V'){
+        console.log('Boa tarde!')
+    } else if(turnoAluno === 'N'){
+    console.log('Boa noite')}
+
+//   3)
+
+    let turnoAlunoswitch = prompt('Digite o seu turno, sendo: M (matutino), V (Vespertino) ou N (Noturno)')
+
+let turnoAlunosswitch
+switch (turnoAlunosr){
+    case "M";
+        console.log('Bom dia')
+
+
+
